ae605417f8d5b9e3d09302f4a5e4f424bf362142
Oscar Fonts <oscar.fonts@gmail.com>
2017-05-22 09:25:58 +0200

    Added grafcanProvider tests

--- test/grafcanProviderSpec.js
+++ test/grafcanProviderSpec.js
@@ -1,7 +1,24 @@
 import Grafcan from '../src/grafcanProvider';
 
-describe('Grafcan control interface', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
+describe('Grafcan Provider', () => {
+
+  it('Should get an array of results from grafcan API', (done) => {
+    let provider = new Grafcan();
+    provider.search({ query: "tenerife" }).then(response => {
+      expect(response.length).toEqual(20);
+      done();
+    });
   });
+
+  it('Should return results with "label", "x" and "y" properties', (done) => {
+    let provider = new Grafcan();
+    provider.search({ query: "tenerife" }).then(response => {
+      let result = response[0];
+      expect(result.label).toEqual(jasmine.any(String));
+      expect(result.x).toEqual(jasmine.any(Number));
+      expect(result.y).toEqual(jasmine.any(Number));
+      done();
+    });
+  });
+
 });
